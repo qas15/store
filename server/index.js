b784ac0de424482a9eb8791d4546e1e37384e1b7
root <root@Roma>
2025-03-19 02:00:14 +0300

    done

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = express()
 app.use(fileUpload());
 app.use(cors({
   origin: '*', 
-  methods: ['GET', 'POST', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json())
