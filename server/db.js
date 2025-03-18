ac6edaef45ea15733378f45946b14fa5bb3318df
root <root@Roma>
2025-03-19 00:32:36 +0300

    Исправил подключение к базе с использованием DATABASE_URL

--- server/db.js
+++ server/db.js
@@ -1,12 +1,14 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-    }
-)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+module.exports = sequelize;
