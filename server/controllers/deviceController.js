f682d4dd5127440a3067ad7e2e01e7175c2bd975
root <root@Roma>
2025-03-19 03:33:49 +0300

    dofrf

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,49 +1,60 @@
-const fs = require('fs');
+const uuid = require('uuid')
 const path = require('path');
-const uuid = require('uuid');
-const { Device, DeviceInfo } = require('../models/models');
+const {Device, DeviceInfo} = require('../models/models')
 const ApiError = require('../error/ApiError');
 
 class DeviceController {
     async create(req, res, next) {
         try {
-            let { name, price, brandId, typeId, info } = req.body;
-            let fileName = null;
-
-            if (req.files?.img) {
-                const { img } = req.files;
-                fileName = uuid.v4() + ".jpg";
-                await img.mv(path.resolve(__dirname, '..', 'static', fileName));
-            }
-
-            const device = await Device.create({ 
-                name, 
-                price, 
-                brandId, 
-                typeId, 
-                img: fileName 
-            });
-
+            let {name, price, brandId, typeId, info} = req.body
+            const {img} = req.files
+            let fileName = uuid.v4() + ".jpg"
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+            const device = await Device.create({name, price, brandId, typeId, img: fileName});
             if (info) {
-                try {
-                    info = JSON.parse(info);
-                    await Promise.all(info.map(i =>
-                        DeviceInfo.create({
-                            title: i.title,
-                            description: i.description,
-                            deviceId: device.id
-                        })
-                    ));
-                } catch (error) {
-                    return next(ApiError.badRequest("Invalid JSON in 'info' field"));
-                }
+                info = JSON.parse(info)
+                info.forEach(i =>
+                    DeviceInfo.create({
+                        title: i.title,
+                        description: i.description,
+                        deviceId: device.id
+                    })
+                )
             }
-
-            return res.json(device);
+            return res.json(device)
         } catch (e) {
-            return next(ApiError.badRequest(e.message));
+            next(ApiError.badRequest(e.message))
         }
     }
+    async getAll(req, res) {
+        let {brandId, typeId, limit, page} = req.query
+        page = page || 1
+        limit = limit || 9
+        let offset = page * limit - limit
+        let devices;
+        if (!brandId && !typeId) {
+            devices = await Device.findAndCountAll({limit, offset})
+        }
+        if (brandId && !typeId) {
+            devices = await Device.findAndCountAll({where:{brandId}, limit, offset})
+        }
+        if (!brandId && typeId) {
+            devices = await Device.findAndCountAll({where:{typeId}, limit, offset})
+        }
+        if (brandId && typeId) {
+            devices = await Device.findAndCountAll({where:{typeId, brandId}, limit, offset})
+        }
+        return res.json(devices)
+    }
+    async getOne(req, res) {
+        const {id} = req.params
+        const device = await Device.findOne(
+            {
+                where: {id},
+                include: [{model: DeviceInfo, as: 'info'}]
+            },
+        )
+        return res.json(device)
+    }
 }
-
-module.exports = new DeviceController();
+module.exports = new DeviceController()
