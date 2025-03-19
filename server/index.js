01c2f0c2e7ac5538e3f9acc15546b8a631fdc76c
root <root@Roma>
2025-03-19 03:35:57 +0300

    dofrffdfg

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ const {Order, OrderDevice} = require("./models/models");
 const PORT = process.env.PORT || 5000
 
 const app = express()
-app.use(fileUpload({}));
 app.use(cors({
   origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
