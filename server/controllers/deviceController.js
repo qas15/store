9ac9615bde63aed2df41a54b77434b1e17a00465
qas15 <102079500+qas15@users.noreply.github.com>
2025-03-19 06:00:48 +0300

    Update deviceController.js

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -9,7 +9,7 @@ class DeviceController {
             let {name, price, brandId, typeId, info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
-            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+            await img.mv(path.resolve(__dirname, '..', 'static', fileName))
             const device = await Device.create({name, price, brandId, typeId, img: fileName});
             if (info) {
                 info = JSON.parse(info)
