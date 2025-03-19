59c8fda37e8b517007a82d0e349c4c6e3de86181
root <root@Roma>
2025-03-19 03:29:22 +0300

    doness

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -8,37 +8,41 @@ class DeviceController {
     async create(req, res, next) {
         try {
             let { name, price, brandId, typeId, info } = req.body;
-            const { img } = req.files;
-            let fileName = uuid.v4() + ".jpg";
-            img.mv(path.resolve(__dirname, '..', 'static', fileName));
-            const device = await Device.create({ name, price, brandId, typeId, img: fileName });
+            let fileName = null;
+
+            if (req.files?.img) {
+                const { img } = req.files;
+                fileName = uuid.v4() + ".jpg";
+                await img.mv(path.resolve(__dirname, '..', 'static', fileName));
+            }
+
+            const device = await Device.create({ 
+                name, 
+                price, 
+                brandId, 
+                typeId, 
+                img: fileName 
+            });
 
             if (info) {
-                info = JSON.parse(info);
-                info.forEach(i =>
-                    DeviceInfo.create({
-                        title: i.title,
-                        description: i.description,
-                        deviceId: device.id
-                    })
-                );
+                try {
+                    info = JSON.parse(info);
+                    await Promise.all(info.map(i =>
+                        DeviceInfo.create({
+                            title: i.title,
+                            description: i.description,
+                            deviceId: device.id
+                        })
+                    ));
+                } catch (error) {
+                    return next(ApiError.badRequest("Invalid JSON in 'info' field"));
+                }
             }
 
             return res.json(device);
         } catch (e) {
-            next(ApiError.badRequest(e.message));
+            return next(ApiError.badRequest(e.message));
         }
-    }
-
-    async getAll(req, res) {
-        const devices = await Device.findAll();
-        return res.json(devices);
-    }
-
-    async getOne(req, res) {
-        const { id } = req.params;
-        const device = await Device.findOne({ where: { id } });
-        return res.json(device);
     }
 }
 
