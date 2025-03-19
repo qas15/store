6b88dffcd916985c232805041549d50b83664fa4
root <root@Roma>
2025-03-19 03:13:00 +0300

    doness

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -7,58 +7,38 @@ const ApiError = require('../error/ApiError');
 class DeviceController {
     async create(req, res, next) {
         try {
-            console.log("Запрос на создание устройства:", req.body);
-
             let { name, price, brandId, typeId, info } = req.body;
-
-            if (!req.files || !req.files.img) {
-                return next(ApiError.badRequest("Файл изображения обязателен"));
-            }
             const { img } = req.files;
-
-            console.log("Файл изображения получен:", img.name);
-
-            const staticPath = path.resolve(__dirname, '..', 'static');
-            if (!fs.existsSync(staticPath)) {
-                fs.mkdirSync(staticPath, { recursive: true });
-            }
-
             let fileName = uuid.v4() + ".jpg";
-            const filePath = path.resolve(staticPath, fileName);
-
-            img.mv(filePath, (err) => {
-                if (err) {
-                    return next(ApiError.badRequest("Ошибка при загрузке изображения"));
-                }
-            });
-
-            console.log("Изображение сохранено успешно!");
-
+            img.mv(path.resolve(__dirname, '..', 'static', fileName));
             const device = await Device.create({ name, price, brandId, typeId, img: fileName });
 
-            console.log("Устройство создано:", device);
-
             if (info) {
-                try {
-                    info = JSON.parse(info);
-
-                    for (const i of info) {
-                        await DeviceInfo.create({
-                            title: i.title,
-                            description: i.description,
-                            deviceId: device.id
-                        });
-                    }
-                    console.log("Информация о девайсе добавлена!");
-                } catch (error) {
-                    return next(ApiError.badRequest("Ошибка парсинга info"));
-                }
+                info = JSON.parse(info);
+                info.forEach(i =>
+                    DeviceInfo.create({
+                        title: i.title,
+                        description: i.description,
+                        deviceId: device.id
+                    })
+                );
             }
 
             return res.json(device);
         } catch (e) {
             next(ApiError.badRequest(e.message));
         }
+    }
+
+    async getAll(req, res) {
+        const devices = await Device.findAll();
+        return res.json(devices);
+    }
+
+    async getOne(req, res) {
+        const { id } = req.params;
+        const device = await Device.findOne({ where: { id } });
+        return res.json(device);
     }
 }
 
